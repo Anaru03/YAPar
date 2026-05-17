a9b6b44ba9edba54e337a352fe13ac1022589274
Anaru03 <dele22428@uvg.edu.gt>
2026-05-16 22:07:34 -0600

    Add utility methods to Production class

--- src/parser/Production.java
+++ src/parser/Production.java
@@ -10,4 +10,22 @@ public class Production {
         this.left = left;
         this.right = new java.util.ArrayList<>(right);
     }
+
+    @Override
+    public String toString() {
+        return left + " -> " + String.join(" ", right);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Production)) return false;
+        Production p = (Production) o;
+        return left.equals(p.left) && right.equals(p.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(left, right);
+    }
 }
