cf8ddf6e71974d7450486afe8e27b60d3c265073
Anaru03 <dele22428@uvg.edu.gt>
2026-05-16 23:15:40 -0600

    Create YAParFileParser base structure

--- src/parser/YAParFileParser.java
+++ src/parser/YAParFileParser.java
@@ -1,5 +1,24 @@
 package parser;
 
+import java.io.*;
+
 public class YAParFileParser {
-    
+
+    public static Grammar parse(String filePath) throws IOException {
+        String content = readFile(filePath);
+        return parseContent(content);
+    }
+
+    public static Grammar parseContent(String content) {
+        return null;
+    }
+
+    private static String readFile(String path) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) sb.append(line).append("\n");
+        }
+        return sb.toString();
+    }
 }
